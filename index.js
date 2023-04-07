908d1bc40fa5a2c36f1b4a835887e8d8d18930b3
mdziurakh <mdzjurakh1@gmail.com>
2023-04-07 13:39:58 +0300

    add slider caption

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const swiper = new Swiper(".top-slider", {
   // Optional parameters
   direction: "horizontal",
   loop: true,
-  autoplay: {
-    delay: 4000,
-  },
+  // autoplay: {
+  //   delay: 4000,
+  // },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
