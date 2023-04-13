70542218476011b6b497f8605604a06e8b91c8e4
mdziurakh <mdzjurakh1@gmail.com>
2023-04-13 17:25:30 +0300

    add main-our-team-section

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-const swiper = new Swiper(".top-slider", {
+const topSwiper = new Swiper(".top-slider", {
   // Optional parameters
   direction: "horizontal",
   loop: true,
   // autoplay: {
   //   delay: 4000,
+  // },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+const ourTeamSwiper = new Swiper(".our-team__slider", {
+  // Optional parameters
+  slidesPerView: 2,
+  direction: "horizontal",
+  loop: true,
+  // autoplay: {
+  //   delay: 230,
   // },
   navigation: {
     nextEl: ".swiper-button-next",
