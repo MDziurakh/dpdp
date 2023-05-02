63e8cbb0af380dd9e2a243badbf4ca5c2203be28
mdziurakh <mdzjurakh1@gmail.com>
2023-05-02 11:11:47 +0300

    add s-google-map

--- index.js
+++ index.js
@@ -24,3 +24,23 @@ const ourTeamSwiper = new Swiper(".our-team__slider", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+// datetime controller
+function getCurrentTime() {
+  const dateControl = document.querySelector("#form-datetime");
+  const today = new Date();
+  let nextHour = today.getHours() + 1;
+  // let minutes = today.getMinutes();
+  if (nextHour.toString().length == 1) {
+    nextHour = "0" + nextHour;
+  }
+  // if(minutes.toString().length == 1){
+  //   minutes = '0'+minutes;
+  // }
+
+  // const time = today.getHours().toString()+today.getMinutes().toString()
+  console.dir(nextHour);
+  console.log(today.toISOString().substring(0, 10) + "");
+  dateControl.min = `${today.toISOString().substring(0, 10) + ""}T14:00`;
+}
+// getCurrentTime(); для форми запису
