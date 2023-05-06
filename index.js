c0da89da6ce2de4f41cb9fa65c15fd8355efe4b8
mdziurakh <mdzjurakh1@gmail.com>
2023-05-06 22:27:01 +0300

    fix header, add responive

--- index.js
+++ index.js
@@ -43,5 +43,37 @@ function getCurrentTime() {
   console.log(today.toISOString().substring(0, 10) + "");
   dateControl.min = `${today.toISOString().substring(0, 10) + ""}T14:00`;
 }
-getCurrentTime(); 
+// getCurrentTime();
 // для форми запису.
+
+window.onload = function () {
+  const mobileMenu = document.querySelector(".mobile-nav-menu");
+  const hamburger = document.querySelector("#hamburger");
+  hamburger.addEventListener("click", () => {
+    mobileMenu.classList.toggle("open");
+    hamburger.classList.toggle("open");
+  });
+
+  const tabs = document.querySelectorAll(".tab");
+  tabs.forEach((tab) => {
+    tab.addEventListener("click", () => {
+      tab.classList.toggle("open");
+    });
+  });
+  document.addEventListener("click", documentActions);
+
+  function documentActions(e) {
+    const targetElement = e.target;
+    if (
+      !targetElement.closest(".menu-item") &&
+      mobileMenu.classList.contains("open") &&
+      !targetElement.closest(".hamburger") &&
+      !targetElement.classList.contains("hamburger")
+    ) {
+      mobileMenu.classList.remove("open");
+      hamburger.classList.remove("open");
+    }
+  
+    // if(targetElement.)
+  }
+};
